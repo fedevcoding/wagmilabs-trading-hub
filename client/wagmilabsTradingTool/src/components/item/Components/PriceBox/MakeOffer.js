25e2523c3ed9b23ec90119dcf83eba5bb9b5a458
Andrea <andreaacanfora95@gmail.com>
2023-02-03 12:58:03 +0100

    Make offer btn

--- client/wagmilabsTradingTool/src/components/item/Components/PriceBox/MakeOffer.js
+++ client/wagmilabsTradingTool/src/components/item/Components/PriceBox/MakeOffer.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { NumberInput, NumberInputField } from "@chakra-ui/react";
 
 export const MakeOffer = React.memo(({ details, address }) => {
-  return (
-    <div className="btn">
+  const [insertOffer, setInsertOffer] = React.useState(false);
+
+  return insertOffer ? (
+    <div className="btn insert-bid">
+      <div className="back" onClick={() => setInsertOffer(!insertOffer)}>
+        <i className="fa fa-arrow-left" />
+      </div>
+      <NumberInput>
+        <NumberInputField placeholder="Insert ETH amount" />
+      </NumberInput>
+      <div className="make-offer">
+        <i className="fa fa-tag" />
+        Confirm
+      </div>
+    </div>
+  ) : (
+    <div className="btn" onClick={() => setInsertOffer(!insertOffer)}>
       <i className="fa fa-tag" />
       Make offer
     </div>
