e979657736bec74f05323f4fbe306b148c7d5f98
Andrea <andreaacanfora95@gmail.com>
2023-02-07 16:03:44 +0100

    Update Footer.js

--- client/wagmilabsTradingTool/src/components/footer/Footer.js
+++ client/wagmilabsTradingTool/src/components/footer/Footer.js
@@ -1,79 +1,102 @@
-import React, { useContext, useState } from 'react'
-import GasModal from '../utility-components/GasModal'
-import LivePulsing from '../utility-components/LivePulsing'
-import "./Footer.css"
+import React, { useContext, useState } from "react";
+import GasModal from "../utility-components/GasModal";
+import LivePulsing from "../utility-components/LivePulsing";
+import { NotificationContainer } from "react-notifications";
+import "./Footer.css";
 
-import { UserDataContext } from '../../context/userContext'
-
+import { UserDataContext } from "../../context/userContext";
 
 const Footer = () => {
-    const { gasSettings, ethData } = useContext(UserDataContext)
+  const { gasSettings, ethData } = useContext(UserDataContext);
 
-    const [gasModalOpen, setGasModalOpen] = useState(false)
+  const [gasModalOpen, setGasModalOpen] = useState(false);
 
-    const currentBlock = ethData?.currentBlockNumber || 0
-    let blockGas = 0
-    if (ethData?.blockPrices) {
-        blockGas = Math.round(ethData?.blockPrices[0].baseFeePerGas)
+  const currentBlock = ethData?.currentBlockNumber || 0;
+  let blockGas = 0;
+  if (ethData?.blockPrices) {
+    blockGas = Math.round(ethData?.blockPrices[0].baseFeePerGas);
+  }
+
+  const openGasSettings = () => {
+    if (!gasModalOpen) {
+      setGasModalOpen(true);
     }
+  };
 
-    const openGasSettings = () => {
-        if (!gasModalOpen) {
-            setGasModalOpen(true)
-        }
-    }
+  return (
+    <>
+      <footer className="global-footer">
+        <div className="footer-eth-settings">
+          <div className="footer-gas-preset" onClick={openGasSettings}>
+            <i className="fa-solid fa-bolt-lightning"></i>
+            <p>Preset: {gasSettings.label}</p>
+            <GasModal
+              setGasModalOpen={setGasModalOpen}
+              gasModalOpen={gasModalOpen}
+            />
+          </div>
 
-    return (
+          <a
+            className="footer-blocks-mined"
+            href={`https://etherscan.io/block/${currentBlock}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-duotone fa-cube"></i>
+            <p>{currentBlock}</p>
+          </a>
 
-        <footer className='global-footer'>
+          <a
+            className="footer-gas-preset"
+            href="https://etherscan.io/gastracker"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <i className="fa-solid fa-gas-pump"></i>
+            <p>{blockGas}</p>
+          </a>
 
+          <a
+            className="footer-gas-preset"
+            href="https://tradingview.com/chart/?symbol=COINBASE%3AETHUSD"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-ethereum"></i>
+            <p>${ethData?.ethPrice}</p>
+          </a>
+          <LivePulsing />
+        </div>
 
-            <div className='footer-eth-settings'>
+        <div className="footer-links">
+          <a href="/legal" target={"_blank"}>
+            <i className="fa-solid fa-scale-unbalanced"></i>
+          </a>
 
+          <a href="#info" target={"_blank"}>
+            <i className="fa-regular fa-circle-info"></i>
+          </a>
 
-                <div className='footer-gas-preset' onClick={openGasSettings}>
-                    <i className="fa-solid fa-bolt-lightning"></i>
-                    <p>Preset: {gasSettings.label}</p>
-                    <GasModal setGasModalOpen={setGasModalOpen} gasModalOpen={gasModalOpen} />
-                </div>
+          <a
+            href="https://discord.gg/wagmilabs"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-discord"></i>
+          </a>
 
-                <a className='footer-blocks-mined' href={`https://etherscan.io/block/${currentBlock}`} target="_blank">
-                    <i className="fa-duotone fa-cube"></i>
-                    <p>{currentBlock}</p>
-                </a>
+          <a
+            href="https://twitter.com/wagmi_labs"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+        </div>
+      </footer>
+      <NotificationContainer />
+    </>
+  );
+};
 
-                <a className='footer-gas-preset' href='https://etherscan.io/gastracker' target={"_blank"}>
-                    <i className="fa-solid fa-gas-pump"></i>
-                    <p>{blockGas}</p>
-                </a>
-
-                <a className='footer-gas-preset' href='https://tradingview.com/chart/?symbol=COINBASE%3AETHUSD' target={"_blank"}>
-                    <i className="fa-brands fa-ethereum"></i>
-                    <p>${ethData?.ethPrice}</p>
-                </a>
-                <LivePulsing />
-
-            </div>
-
-            <div className='footer-links'>
-                <a href='/legal' target={"_blank"}>
-                    <i className="fa-solid fa-scale-unbalanced"></i>
-                </a>
-
-                <a href='' target={"_blank"}>
-                    <i className="fa-regular fa-circle-info"></i>
-                </a>
-
-                <a href='https://discord.gg/wagmilabs' target={"_blank"}>
-                    <i className="fa-brands fa-discord"></i>
-                </a>
-
-                <a href='https://twitter.com/wagmi_labs' target={"_blank"}>
-                    <i className="fa-brands fa-twitter"></i>
-                </a>
-            </div>
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
