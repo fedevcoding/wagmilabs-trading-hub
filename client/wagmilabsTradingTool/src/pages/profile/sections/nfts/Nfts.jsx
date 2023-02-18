58c500d69d5756d51dd6352ef3a9f6570e461d90
GitFede100 <federicocavallini100@gmail.com>
2023-02-18 21:56:02 +0100

    logo beta

--- client/wagmilabsTradingTool/src/pages/profile/sections/nfts/Nfts.jsx
+++ client/wagmilabsTradingTool/src/pages/profile/sections/nfts/Nfts.jsx
@@ -50,10 +50,10 @@ const Nfts = ({
 
 
   useEffect(() => {
-    if(TransferModalDetails) setIsOpenTransferModal(true)
+    if (TransferModalDetails) setIsOpenTransferModal(true)
     else setIsOpenTransferModal(false)
   }, [TransferModalDetails])
-  
+
   const { cryptoPrices } = useContext(UserDataContext);
   const toast = useToast();
 
@@ -162,19 +162,19 @@ const Nfts = ({
   const activateList = index => {
     document
       .querySelectorAll(".profile-list-nft")
-      [index].classList.remove("inactive");
+    [index].classList.remove("inactive");
     document
       .querySelectorAll(".profile-list-nft")
-      [index].classList.add("active");
+    [index].classList.add("active");
   };
 
   const deactivateList = index => {
     document
       .querySelectorAll(".profile-list-nft")
-      [index].classList.remove("active");
+    [index].classList.remove("active");
     document
       .querySelectorAll(".profile-list-nft")
-      [index].classList.add("inactive");
+    [index].classList.add("inactive");
   };
 
   const toggleOptions = index => {
@@ -328,21 +328,19 @@ const Nfts = ({
               onMouseOut={() => deactivateList(index)}
             >
               <div
-                className={`${
-                  selectBulk
-                    ? "profile-items-details-container-bulk"
-                    : "profile-items-details-container"
-                }`}
+                className={`${selectBulk
+                  ? "profile-items-details-container-bulk"
+                  : "profile-items-details-container"
+                  }`}
               >
                 <div className="image-hover-overflow">
                   <img
                     src={image}
                     alt=""
-                    className={`profile-single-item-image ${
-                      selectBulk
-                        ? "single-item-image"
-                        : "single-item-image-scale"
-                    }`}
+                    className={`profile-single-item-image ${selectBulk
+                      ? "single-item-image"
+                      : "single-item-image-scale"
+                      }`}
                     onClick={e =>
                       selectBulk
                         ? changeBulkItems(contractAddress, tokenId, id, e)
@@ -444,17 +442,17 @@ const Nfts = ({
 
   return (
     <>
-     {
-      isOpenTransferModal && (
-        <TransferItemModal
-          details={TransferModalDetails?.details}
-          id={TransferModalDetails?.id}
-          address={TransferModalDetails?.address}
-          currency={TransferModalDetails?.currency}
-          
-          isOpen={true}
-          setIsOpen={setIsOpenTransferModal}
-        />
+      {
+        isOpenTransferModal && (
+          <TransferItemModal
+            details={TransferModalDetails?.details}
+            id={TransferModalDetails?.id}
+            address={TransferModalDetails?.address}
+            currency={TransferModalDetails?.currency}
+
+            isOpen={true}
+            setIsOpen={setIsOpenTransferModal}
+          />
         )
       }
 
@@ -476,9 +474,8 @@ const Nfts = ({
             <div className="profile-sort-items-option">
               {selectedSortOption.label}
               <i
-                className={`fa-solid fa-caret-down profile-collection-sort-arrow ${
-                  showSortItemsOptions && "selected"
-                }`}
+                className={`fa-solid fa-caret-down profile-collection-sort-arrow ${showSortItemsOptions && "selected"
+                  }`}
               ></i>
             </div>
 
@@ -530,9 +527,8 @@ const Nfts = ({
 
                     return (
                       <div
-                        className={`single-collection-dropdown ${
-                          address === nftsCollectionFilter && "selected"
-                        }`}
+                        className={`single-collection-dropdown ${address === nftsCollectionFilter && "selected"
+                          }`}
                         key={index}
                         active="false"
                         onClick={e => handleFilterCollection(address, e)}
@@ -628,7 +624,7 @@ const Nfts = ({
 
           {userItems.length === 0 ? (
             <div className="profile-user-no-items">
-              <img src={notFound} alt="" />
+              <img src={notFound} alt="not-found" />
               <p>No NFTs found.</p>
             </div>
           ) : (
--- server/routes/ethereum/trendingRoute.js
+++ server/routes/ethereum/trendingRoute.js
@@ -1,72 +1,66 @@
-const express = require("express")
-const checkAuth = require("../../middleware/checkAuth")
-const Ranking = require("../../models/RankingModel")
+const express = require("express");
+const checkAuth = require("../../middleware/checkAuth");
+const Ranking = require("../../models/RankingModel");
 
+const trendingRoute = express();
 
-const trendingRoute = express()
+trendingRoute.get("/:time", checkAuth, (req, res) => {
+  async function getData() {
+    try {
+      const { time } = req.params;
+      const userTime = parseInt(time);
 
-trendingRoute.get('/:time', checkAuth, (req, res) => {
-    
-    async function getData(){
+      const rightTime = new Date().getTime() - userTime;
 
-        try{
-            const {time} = req.params
-            const userTime = parseInt(time)
+      const trendingCollections = await Ranking.aggregate([
+        {
+          $unwind: "$sales",
+        },
+        {
+          $match: {
+            "sales.saleTime": { $gt: rightTime },
+            "sales.value": { $gt: 0 },
+          },
+        },
+        {
+          $group: {
+            _id: "$_id",
+            volume: { $sum: "$sales.value" },
+            firstDoc: { $first: "$$ROOT" },
+            rightSales: { $sum: 1 },
+          },
+        },
+        {
+          $match: { rightSales: { $gt: 0 } },
+        },
+        {
+          $addFields: {
+            "firstDoc.volume": "$volume",
+            "firstDoc.rightSales": "$rightSales",
+          },
+        },
+        {
+          $replaceRoot: {
+            newRoot: "$firstDoc",
+          },
+        },
+        {
+          $sort: {
+            rightSales: -1,
+            volume: -1,
+          },
+        },
+        {
+          $limit: 50,
+        },
+      ]);
 
-            const rightTime = new Date().getTime() - userTime
-        
-            const trendingCollections = await Ranking.aggregate([
-                {
-                    $unwind:"$sales"
-                },
-                {
-                    $match:{
-                        "sales.saleTime":{$gt: rightTime},
-                        "sales.value":{$gt:0}
-                    }
-                },
-                {
-                    $group:{
-                        _id:"$_id",
-                        volume:{$sum:'$sales.value'},
-                        firstDoc: { $first: "$$ROOT" },
-                        rightSales:{$sum:1},
-                    }
-                },
-                {
-                    $match: {"rightSales": {"$gt": 0}}
-                },
-                {
-                    $addFields: {
-                        "firstDoc.volume": "$volume",
-                        "firstDoc.rightSales": "$rightSales",
-                    }
-                },
-                {
-                    $replaceRoot: {
-                        newRoot: "$firstDoc"
-                    }
-                },
-                {
-                    $sort: {
-                        "rightSales": -1,
-                        "volume": -1,
-                    }
-                },
-                {
-                    $limit: 50
-                }
-            ])
-
-            res.status(200).json({trendingCollections, time})
-        }
-        catch(e){
-            res.status(500).json({error: e})
-        }
+      res.status(200).json({ trendingCollections, time });
+    } catch (e) {
+      res.status(500).json({ error: e });
     }
-    getData()
+  }
+  getData();
+});
 
-})
-
-module.exports = trendingRoute
-// module.exports = getData
+module.exports = trendingRoute;
