a5619ae5523f9e35bd94ae723f870115dcf8cd04
GitFede100 <federicocavallini100@gmail.com>
2023-02-21 20:40:08 +0100

    updates

--- client/wagmilabsTradingTool/src/pages/collection/sections/liveView/components/BubbleChart.jsx
+++ client/wagmilabsTradingTool/src/pages/collection/sections/liveView/components/BubbleChart.jsx
@@ -5,23 +5,74 @@ import HighCharts from 'highcharts'
 
 const BubbleChart = memo(({ totalListings, totalSales, floorPrice }) => {
 
+    let maxPrice = 0
+    if (floorPrice < 0.005) maxPrice = 0.04
+    else if (floorPrice < 0.01) maxPrice = 0.08
+    else if (floorPrice < 0.05) maxPrice = 0.2
+    else if (floorPrice < 0.1) maxPrice = 0.4
+    else if (floorPrice < 0.2) maxPrice = 0.5
+    else if (floorPrice < 0.4) maxPrice = 0.7
+    else if (floorPrice < 0.7) maxPrice = 1.1
+    else if (floorPrice < 1) maxPrice = 1.4
+    else if (floorPrice < 5) maxPrice = 8
+    else if (floorPrice < 10) maxPrice = 18
 
-    const listingData = totalListings?.map(listing => listing.value <= floorPrice + 0.05 && [new Date(listing.timestamp * 1000), listing.value])
-    const salesData = totalSales?.map(sale => sale.value <= floorPrice + 0.1 && [new Date(sale.timestamp), sale.value])
+    const listingData = totalListings?.map(listing => listing.value < maxPrice && ({ x: listing.timestamp * 1000, y: listing.value, name: listing.name, image: listing.image }))
+    const salesData = totalSales?.map(sale => sale.value < maxPrice && ({ x: sale.timestamp, y: sale.value, name: sale.name, image: sale.image }))
+
 
     const chartOptions = {
         series: [
             {
                 type: "scatter",
                 name: "Sales",
+                turboThreshold: 2500,
                 data: salesData,
+                tooltip: {
+                    pointFormat: `
+                    <div>
+                        <p>{new Date(point.x)}</p>
+                        <p>{point.name}</p>
+                        <img class="bubble-chart-image" src={point.image} style={{width: 50px, height: 50px}} />
+                    </div>
+                    `,
+                },
+                marker: {
+                    fillColor: "red",
+                    lineColor: "#FFFFFF",
+                    zIndex: 100,
+                },
+
             },
             {
                 type: "scatter",
                 name: "Listings",
                 data: listingData,
+                turboThreshold: 2500,
+                tooltip: {
+                    pointFormat: `
+                    <div>
+                        <p>{new Date(point.x)}</p>
+                        <p>{point.name}</p>
+                        <img src={point.image} style={{width: 50px, height: 50px}} />
+                    </div>
+                    `,
+                },
+                marker: {
+                    fillColor: "orange",
+                    lineColor: "#FFFFFF",
+                    zIndex: 90,
+                },
             },
         ],
+        plotOptions: {
+            series: {
+                marker: {
+                    symbol: "circle",
+                    radius: 4,
+                },
+            },
+        },
         title: {
             text: "",
         },
@@ -29,19 +80,16 @@ const BubbleChart = memo(({ totalListings, totalSales, floorPrice }) => {
             type: "datetime",
         },
         chart: {
-            type: "spline",
             backgroundColor: "transparent",
             borderRadius: 10,
         },
         tooltip: {
-            shared: true,
-            hideDelay: 200,
-            outside: false,
-        },
+            useHTML: true
+        }
     }
 
     return (
-        <div>
+        <div className='bubble-chart'>
             <HighchartsReact
                 highcharts={HighCharts}
                 options={chartOptions}
