80a117f47bb38f3ddbc0ad8e17e0641c04c8b22d
Andrea <andreaacanfora95@gmail.com>
2023-02-23 11:50:37 +0100

    Removing warnings

--- client/wagmilabsTradingTool/src/pages/collection/sections/liveView/components/ComparisonChart.jsx
+++ client/wagmilabsTradingTool/src/pages/collection/sections/liveView/components/ComparisonChart.jsx
@@ -1,127 +1,148 @@
-import React, { memo, useEffect } from 'react'
-import HighchartsReact from 'highcharts-react-official'
-import HighCharts from 'highcharts'
-import HC_more from "highcharts/highcharts-more"
-import { HStack, NumberInput, NumberInputField, Select } from '@chakra-ui/react'
+import React, { memo, useEffect } from "react";
+import HighchartsReact from "highcharts-react-official";
+import HighCharts from "highcharts";
+import HC_more from "highcharts/highcharts-more";
+import {
+  HStack,
+  NumberInput,
+  NumberInputField,
+  Select,
+} from "@chakra-ui/react";
 
 const ComparisonChart = memo(({ totalListings, totalSales, floorPrice }) => {
+  HC_more(HighCharts);
 
-    HC_more(HighCharts)
+  const [chartType, setChartType] = React.useState("column");
 
-    const [chartType, setChartType] = React.useState("column")
+  const [maxPrice, setMaxPrice] = React.useState(
+    floorPrice + (floorPrice / 100) * 20
+  );
+  const [minPrice] = React.useState(0);
 
-    const [maxPrice, setMaxPrice] = React.useState(floorPrice + floorPrice / 100 * 20)
-    const [minPrice] = React.useState(0)
+  const [timeframe, setTimeframe] = React.useState(Date.now() - 60000);
 
-    const [timeframe, setTimeframe] = React.useState(Date.now() - 60000)
+  const [chartOptions, setChartOptions] = React.useState({});
 
-    const [chartOptions, setChartOptions] = React.useState({})
+  useEffect(() => {
+    const rightSales = totalSales.filter(
+      sale =>
+        sale.value < maxPrice &&
+        sale.value > minPrice &&
+        sale.timestamp >= timeframe
+    ).length;
+    const rightListings = totalListings.filter(
+      listing =>
+        listing.value < maxPrice &&
+        listing.value > minPrice &&
+        listing.timestamp * 1000 >= timeframe
+    ).length;
 
+    const chartOptions1 = {
+      chart: {
+        type: "bubble",
+        plotBorderWidth: 1,
+        height: "50%",
+      },
+      title: {
+        text: "",
+      },
+      tooltip: {
+        useHTML: true,
+        pointFormat: "Listings: {point.z}",
+        followPointer: true,
+      },
+      series: [
+        {
+          name: "Listings",
+          data: [[0, 0, rightListings]],
+        },
+        {
+          name: "Sales",
+          data: [[1, 0, rightSales]],
+        },
+      ],
+      legend: {
+        enabled: false,
+      },
+    };
 
-    useEffect(() => {
-        const rightSales = totalSales.filter(sale => sale.value < maxPrice && sale.value > minPrice && sale.timestamp >= timeframe).length
-        const rightListings = totalListings.filter(listing => listing.value < maxPrice && listing.value > minPrice && listing.timestamp * 1000 >= timeframe).length
+    const chartOptions2 = {
+      chart: {
+        type: "column",
+        backgroundColor: "transparent",
+        height: "50%",
+      },
+      title: {
+        text: "",
+      },
+      yAxis: {
+        text: "",
+      },
+      series: [
+        {
+          name: "Listings",
+          data: [rightListings],
+        },
+        {
+          name: "Sales",
+          data: [rightSales],
+        },
+      ],
+      legend: {
+        enabled: false,
+      },
+    };
 
-        const chartOptions1 = {
-            chart: {
-                type: 'bubble',
-                plotBorderWidth: 1,
-                height: "50%"
+    setChartOptions(chartType === "column" ? chartOptions2 : chartOptions1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [totalListings, totalSales, timeframe, chartType]);
 
-            },
-            title: {
-                text: '',
-            },
-            tooltip: {
-                useHTML: true,
-                pointFormat: 'Listings: {point.z}',
-                followPointer: true,
-            },
-            series: [{
-                name: 'Listings',
-                data: [
-                    [0, 0, rightListings]
-                ],
-            },
-            {
-                name: 'Sales',
-                data: [
-                    [1, 0, rightSales]
-                ],
-            }],
-            legend: {
-                enabled: false,
-            },
-        }
+  return (
+    <div className="sale-list-comparison-chart">
+      <div className="options-container">
+        <HStack>
+          <Select
+            onChange={e => setTimeframe(Date.now() - e.target.value)}
+            color="white"
+            colorScheme={"white"}
+          >
+            <option value={60000}>1 minute</option>
+            <option value={300000}>5 minutes</option>
+            <option value={600000}>10 minutes</option>
+          </Select>
+        </HStack>
 
-        const chartOptions2 = {
-            chart: {
-                type: 'column',
-                backgroundColor: "transparent",
-                height: "50%"
-
-            },
-            title: {
-                text: '',
-            },
-            yAxis: {
-                text: ""
-            },
-            series: [{
-                name: 'Listings',
-                data: [
-                    rightListings
-                ],
-            },
-            {
-                name: 'Sales',
-                data: [
-                    rightSales
-                ],
-            }],
-            legend: {
-                enabled: false,
-            },
-        }
-
-
-        setChartOptions(chartType === "column" ? chartOptions2 : chartOptions1)
-    }, [totalListings, totalSales, timeframe, chartType])
-
-
-
-
-    return (
-        <div className='sale-list-comparison-chart'>
-
-            <div className='options-container'>
-                <HStack>
-                    <Select onChange={e => setTimeframe(Date.now() - e.target.value)} color="white" colorScheme={"white"}>
-                        <option value={60000}>1 minute</option>
-                        <option value={300000}>5 minutes</option>
-                        <option value={600000}>10 minutes</option>
-                    </Select>
-                </HStack>
-
-                <NumberInput>
-                    <HStack>
-                        <NumberInputField placeholder="% above floor" onChange={e => setMaxPrice(floorPrice + floorPrice / 100 * e.target.value)} defaultValue={maxPrice} />
-                    </HStack>
-                </NumberInput>
-
-                <div className='chart-type-selector'>
-                    <i className={`fa-solid fa-chart-simple ${chartType === "column" && "selected"}`} onClick={() => setChartType("column")}></i>
-                    <i className={`fa-sharp fa-solid fa-chart-scatter-bubble ${chartType === "bubble" && "selected"}`} onClick={() => setChartType("bubble")}></i>
-                </div>
-            </div>
-
-
-            <HighchartsReact
-                highcharts={HighCharts}
-                options={chartOptions}
+        <NumberInput>
+          <HStack>
+            <NumberInputField
+              placeholder="% above floor"
+              onChange={e =>
+                setMaxPrice(floorPrice + (floorPrice / 100) * e.target.value)
+              }
+              defaultValue={maxPrice}
             />
-        </div>
-    )
-})
+          </HStack>
+        </NumberInput>
 
-export default ComparisonChart
+        <div className="chart-type-selector">
+          <i
+            className={`fa-solid fa-chart-simple ${
+              chartType === "column" && "selected"
+            }`}
+            onClick={() => setChartType("column")}
+          ></i>
+          <i
+            className={`fa-sharp fa-solid fa-chart-scatter-bubble ${
+              chartType === "bubble" && "selected"
+            }`}
+            onClick={() => setChartType("bubble")}
+          ></i>
+        </div>
+      </div>
+
+      <HighchartsReact highcharts={HighCharts} options={chartOptions} />
+    </div>
+  );
+});
+
+export default ComparisonChart;
