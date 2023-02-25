e5c2aa8975affe45731ef0914c82d36dca09984e
Federico <federicocavallini100@gmail.com>
2023-02-25 18:51:20 +0100

    fixes

--- server/services/externalAPI/reservoirApi.js
+++ server/services/externalAPI/reservoirApi.js
@@ -1,9 +1,11 @@
 require("dotenv").config()
 
 const RESERVOIR_API_KEY = process.env.RESERVOIR_API_KEY;
+const CUSTOM_API = process.env.CUSTOM_RESERVOIR_API
 
-
-const execReservoirApi = async (url) => {
+const execReservoirApi = async (url, custom) => {
+    let apiKey = RESERVOIR_API_KEY
+    if(custom) apiKey = CUSTOM_API
     let result = await fetch(url, {
         headers: {
             'x-api-key': RESERVOIR_API_KEY
