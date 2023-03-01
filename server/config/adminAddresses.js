1bcaa3aecd18959cc085d8da0229d2c7bba7918d
Federico <federicocavallini100@gmail.com>
2023-03-01 22:51:53 +0100

    admin addresses middleware

---
+++ server/config/adminAddresses.js
@@ -0,0 +1,5 @@
+export const adminAddresses = [
+  "0x8d50ca23bddfca6db5ae6de31ca0e6a17586e5b8",
+  "0xfe697c5527ab86daa1e4c08286d2be744a0e321e",
+  "0x7fac7b0161143acfd80257873fb9cdb3f316c10c",
+];
--- server/routes/ethereum/calendars/personalRoute.js
+++ server/routes/ethereum/calendars/personalRoute.js
@@ -1,69 +1,67 @@
-const express = require("express")
-const checkAuth = require("../../../middleware/checkAuth")
+const express = require("express");
+const checkAdmin = require("../../../middleware/checkAdmin");
+const checkAuth = require("../../../middleware/checkAuth");
 const Personal = require("../../../models/calendars/PersonalModel");
 const nullAddress = "0x0000000000000000000000000000000000000000";
-const adminAddresses = [
-    "0x8d50Ca23bDdFCA6DB5AE6dE31ca0E6A17586E5B8",
-    "0xfe697C5527ab86DaA1e4c08286D2bE744a0E321E",
-    "0x7FAC7b0161143Acfd80257873FB9cDb3F316C10C",
-  ];
-const personalRoute = express()
+const personalRoute = express();
 
 personalRoute.get("/", checkAuth, async (req, res) => {
-    const { address } = req.userDetails;
+  const { address } = req.userDetails;
 
-    try {
+  try {
+    const personal = await Personal.find({
+      $or: [{ address: nullAddress }, { address }],
+    });
 
-        const personal = await Personal.find({
-            $or: [
-                {address: nullAddress},
-                {address},
-            ]
-        });
+    if (!personal) throw new Error("Personal not found");
 
-        if (!personal) throw new Error("Personal not found")
-
-        return res.status(200).json({personal})
-    }
-    catch (e) {
-        return res.status(400).json({ msg: e.message });
-    }
-
-})
-
-personalRoute.post('/', checkAuth, async (req, res) => {
-    const { address } = req.userDetails;
-    const isAdmin = adminAddresses.includes(address);
-    const { event } = req.body || {};
-
-    try {
-        const personal = await Personal.updateOne({ address: isAdmin ? nullAddress : address }, { $push: { events: {$each: [event], $slice: -150} } }, { upsert: true })
-        if (!personal) throw Error('Something went wrong saving the admin personal event');
-        
-        return res.status(200).json({personal});
-    } catch (e) {
-        res.status(400).json({ msg: e.message });
-    }
+    return res.status(200).json({ personal });
+  } catch (e) {
+    return res.status(400).json({ msg: e.message });
+  }
 });
 
-personalRoute.delete('/', checkAuth, async (req, res) => {
+personalRoute.post("/", checkAuth, checkAdmin, async (req, res) => {
+  const { address } = req.userDetails;
+  const isAdmin = req.isAdmin;
+  const { event } = req.body || {};
 
-    const { address } = req.userDetails;
-    const isAdmin = adminAddresses.includes(address);
+  try {
+    const personal = await Personal.updateOne(
+      { address: isAdmin ? nullAddress : address },
+      { $push: { events: { $each: [event], $slice: -150 } } },
+      { upsert: true }
+    );
+    if (!personal)
+      throw Error("Something went wrong saving the admin personal event");
 
-    const { id } = req.body || {};
-
-    try {
-        const personal = await Personal.findOneAndUpdate({address: isAdmin ? nullAddress : address}, {
-            $pull: { 
-                events: { _id: id }
-              }} );
-        if (!personal) throw Error('Something went wrong deleting all the personal events');
-        return res.status(200).json({personal});
-    } catch (e) {
-        return res.status(400).json({ msg: e.message });
-    }
+    return res.status(200).json({ personal });
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
 });
 
+personalRoute.delete("/", checkAuth, async (req, res) => {
+  const { address } = req.userDetails;
+  const isAdmin = req.isAdmin;
 
-module.exports = personalRoute
+  const { id } = req.body || {};
+
+  try {
+    const personal = await Personal.findOneAndUpdate(
+      { address: isAdmin ? nullAddress : address },
+      {
+        $pull: {
+          events: { _id: id },
+        },
+      }
+    );
+    if (!personal)
+      throw Error("Something went wrong deleting all the personal events");
+    return res.status(200).json({ personal });
+  } catch (e) {
+    return res.status(400).json({ msg: e.message });
+  }
+});
+
+module.exports = personalRoute;
