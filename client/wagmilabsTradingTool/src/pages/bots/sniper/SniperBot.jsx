f78a84e5bfd8736ef5efec2d20153f4c0ce4e690
Federico <federicocavallini100@gmail.com>
2023-03-05 17:57:19 +0100

    redeploy

--- client/wagmilabsTradingTool/src/pages/bots/sniper/SniperBot.jsx
+++ client/wagmilabsTradingTool/src/pages/bots/sniper/SniperBot.jsx
@@ -24,6 +24,7 @@ const SniperBot = () => {
     return () => {
       socket.emit("leavePendingSnipes", address);
     };
+    //
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
