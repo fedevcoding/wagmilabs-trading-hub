8ed1f8aa5cb20f9449e1e7016c10531752ce27a1
Andrea <andreaacanfora95@gmail.com>
2023-03-08 18:38:58 +0100

    Profitability calculator

---
+++ client/wagmilabsTradingTool/src/components/Modals/ProfitCalcModal/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+
+import "./style.scss";
+
+export const ProfitCalcModal = React.memo(({ isOpen, setIsOpen }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} size="full">
+      <ModalOverlay />
+      <ModalContent className="profit-calc-modal">
+        <ModalHeader>Profitability calculator</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <iframe src="https://profitcalc.wagmilabs.tools/" title="Profit Calc" />
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={() => setIsOpen(false)}>Close</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+});
