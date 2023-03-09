1b2ae24d4299de5327df11081fed3bde8a3ede36
Andrea <andreaacanfora95@gmail.com>
2023-03-09 17:28:49 +0100

    Img no holders

--- client/wagmilabsTradingTool/src/pages/collection/sections/leaderboard/Leaderboard.jsx
+++ client/wagmilabsTradingTool/src/pages/collection/sections/leaderboard/Leaderboard.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { Tooltip } from "@chakra-ui/react";
 import { Card, LoadingSpinner } from "@Components";
-import { formatAddress, roundPrice2, roundPriceUsd } from "@Utils";
 import { useGetData } from "./useGetData";
-import { useCopy } from "@Hooks";
-import { Filters } from "./Components";
+import { Filters, Row } from "./Components";
 import { useFilters } from "./useFilters";
+import { notFound } from "@Assets";
 
 import "./style.scss";
 
 const Leaderboard = React.memo(({ address }) => {
   const filters = useFilters();
   const { holders, loading } = useGetData(address, filters.sort.value, filters.direction.value);
-  const { copyState, copyAddress } = useCopy();
-
-  console.log("holders", holders, filters);
 
   return (
     <div id="leaderboard">
@@ -23,89 +19,65 @@ const Leaderboard = React.memo(({ address }) => {
           <LoadingSpinner />
         ) : (
           <>
-            <Filters {...filters} />
-            <table>
-              <thead>
-                <tr>
-                  <th width="30" />
-                  <th>Address</th>
-                  <th>Total gains</th>
-                  <th>Num txs</th>
-                  <th>Assets owned</th>
-                  <th>Bluchip owned</th>
-                  <th>Collection owned</th>
-                  <th>Portfolio value</th>
-                  <th>
-                    <Tooltip
-                      label={"All time"}
-                      closeOnClick={false}
-                      hasArrow
-                      fontSize="xs"
-                      bg="black"
-                      color={"white"}
-                      placement="top"
-                      borderRadius={"7px"}
-                    >
-                      Collection gains
-                    </Tooltip>
-                  </th>
-                  <th>
-                    <Tooltip
-                      label={"All time"}
-                      closeOnClick={false}
-                      hasArrow
-                      fontSize="xs"
-                      bg="black"
-                      color={"white"}
-                      placement="top"
-                      borderRadius={"7px"}
-                    >
-                      Collection volume
-                    </Tooltip>
-                  </th>
-                  <th>Collection assets owned</th>
-                </tr>
-              </thead>
-              <tbody>
-                {holders.map((h, i) => (
-                  <tr key={JSON.stringify(h)}>
-                    <td width="30">{i + 1}</td>
-                    <td>
-                      <Tooltip
-                        label={copyState}
-                        closeOnClick={false}
-                        hasArrow
-                        fontSize="xs"
-                        bg="black"
-                        color={"white"}
-                        placement="top"
-                        borderRadius={"7px"}
-                      >
-                        <p className="address" onClick={() => copyAddress(h.address)}>
-                          {formatAddress(h.address)}
-                        </p>
-                      </Tooltip>
-                    </td>
-                    <td>
-                      <i className="fa-brands fa-ethereum" /> {roundPrice2(h.total_gain)}
-                    </td>
-                    <td>{h.num_txs}</td>
-                    <td>{h.num_assets_owned}</td>
-                    <td>{h.num_blue_chips_owned}</td>
-                    <td>{h.num_collections_owned}</td>
-                    <td>
-                      <i className="fa-brands fa-ethereum" /> {roundPrice2(h.portfolio_value_wei / 1e18)}
-                      <small>({roundPriceUsd(h.portfolio_value_usd)}$)</small>
-                    </td>
-                    <td>{h.collection_gains_all_time}</td>
-                    <td>
-                      <i className="fa-brands fa-ethereum" /> {roundPrice2(h.collection_volume_wei_all_time / 1e18)}
-                    </td>
-                    <td>{h.collection_assets_owned}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            {holders.length ? (
+              <>
+                <Filters {...filters} />
+                <table>
+                  <thead>
+                    <tr>
+                      <th width="30" />
+                      <th>Address</th>
+                      <th>Total gains</th>
+                      <th>Num txs</th>
+                      <th>Assets owned</th>
+                      <th>Bluchip owned</th>
+                      <th>Collection owned</th>
+                      <th>Portfolio value</th>
+                      <th>
+                        <Tooltip
+                          label={"All time"}
+                          closeOnClick={false}
+                          hasArrow
+                          fontSize="xs"
+                          bg="black"
+                          color={"white"}
+                          placement="top"
+                          borderRadius={"7px"}
+                        >
+                          Collection gains
+                        </Tooltip>
+                      </th>
+                      <th>
+                        <Tooltip
+                          label={"All time"}
+                          closeOnClick={false}
+                          hasArrow
+                          fontSize="xs"
+                          bg="black"
+                          color={"white"}
+                          placement="top"
+                          borderRadius={"7px"}
+                        >
+                          Collection volume
+                        </Tooltip>
+                      </th>
+                      <th>Collection assets owned</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {holders.map((h, i) => (
+                      <Row h={h} i={i} />
+                    ))}
+                  </tbody>
+                </table>
+              </>
+            ) : (
+              <h3 className="text-center no-holders">
+                <img src={notFound} alt="holders" width={150} />
+                <br />
+                No holders found for this collection!
+              </h3>
+            )}
           </>
         )}
       </Card>
