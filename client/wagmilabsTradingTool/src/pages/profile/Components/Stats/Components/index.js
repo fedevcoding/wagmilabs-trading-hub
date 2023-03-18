e4d6d24d456c6b38afbee73df3a170c585507a8b
Andrea <andreaacanfora95@gmail.com>
2023-03-18 10:55:45 +0100

    stats component refactor

---
+++ client/wagmilabsTradingTool/src/pages/profile/Components/Stats/Components/index.js
@@ -0,0 +1,1 @@
+export { NftDistribution } from "./NftDistribution";
--- client/wagmilabsTradingTool/src/pages/profile/Components/Stats/index.jsx
+++ client/wagmilabsTradingTool/src/pages/profile/Components/Stats/index.jsx
@@ -1,32 +1,18 @@
-import { Card, LoadingSpinner, PieChart } from "@Components";
-import { useGetHoldingNftDistribution } from "@Hooks";
 import React from "react";
+import { Card } from "@Components";
+import { NftDistribution } from "./Components";
 
 import "./style.scss";
 
 export const Stats = () => {
-  const { loading, distribution, tokenCount } = useGetHoldingNftDistribution();
-
-  console.log(loading, distribution, tokenCount);
-
   return (
     <div className="row" id="stats">
+      <NftDistribution />
       <div className="col">
         <Card>
-          <h3>Holding NFT Distribution</h3>
-          {(loading && <LoadingSpinner />) || (
-            <PieChart
-              title={"Distribution"}
-              countElements={tokenCount}
-              items={Object.values(distribution).map(d => ({
-                name: d.name,
-                y: d.count,
-              }))}
-            />
-          )}
+          <h3>Traded Distribution</h3>
         </Card>
       </div>
-      <div className="col"></div>
     </div>
   );
 };
