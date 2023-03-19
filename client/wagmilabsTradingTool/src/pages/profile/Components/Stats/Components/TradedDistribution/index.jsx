922649b53f20e252baf9414a2a26392727c963b0
Andrea <andreaacanfora95@gmail.com>
2023-03-19 17:03:01 +0100

    Use filter hook

--- client/wagmilabsTradingTool/src/pages/profile/Components/Stats/Components/TradedDistribution/index.jsx
+++ client/wagmilabsTradingTool/src/pages/profile/Components/Stats/Components/TradedDistribution/index.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import { Card, LoadingSpinner, Tabs } from "@Components";
-import { useGetTradedNftDistribution } from "@Hooks";
 import Chart from "./Chart";
 import { HStack, NumberInput, NumberInputField } from "@chakra-ui/react";
+import { useFilters } from "./useFilters";
 
 export const TradedDistribution = React.memo(() => {
-  const periods = {
-    "7d": 7,
-    "30d": 30,
-    "3M": 90,
-    "1y": 365,
-    all: 1800,
-  };
-  const [currentPeriod, setPeriod] = React.useState("30d");
-
-  const { loading, nfts } = useGetTradedNftDistribution(periods[currentPeriod]);
-
-  const items = nfts.map(n => ({
-    ...n,
-    price: n.price && n.type === "mint" ? n.price / 10 ** 18 : n.price,
-  }));
-
-  const [maxPrice, setMaxPrice] = React.useState(undefined);
-  const [minPrice, setMinPrice] = React.useState(0);
+  const { items, loading, periods, currentPeriod, maxPrice, minPrice, setMaxPrice, setMinPrice, setPeriod } =
+    useFilters();
 
   return (
     <div className="col">
@@ -36,7 +20,6 @@ export const TradedDistribution = React.memo(() => {
                 <NumberInputField width={50} onChange={e => setMinPrice(e.target.value)} />
               </NumberInput>
             </HStack>
-
             <HStack>
               <label>Max price: </label>
               <NumberInput min={0} value={maxPrice}>
---
+++ client/wagmilabsTradingTool/src/pages/profile/Components/Stats/Components/TradedDistribution/useFilters.js
@@ -0,0 +1,35 @@
+import { useGetTradedNftDistribution } from "@Hooks";
+import React from "react";
+
+export function useFilters() {
+  const periods = {
+    "7d": 7,
+    "30d": 30,
+    "3M": 90,
+    "1y": 365,
+    all: 1800,
+  };
+  const [currentPeriod, setPeriod] = React.useState("30d");
+
+  const { loading, nfts } = useGetTradedNftDistribution(periods[currentPeriod]);
+
+  const items = nfts.map(n => ({
+    ...n,
+    price: n.price && n.type === "mint" ? n.price / 10 ** 18 : n.price,
+  }));
+
+  const [maxPrice, setMaxPrice] = React.useState(undefined);
+  const [minPrice, setMinPrice] = React.useState(0);
+
+  return {
+    items,
+    loading,
+    periods,
+    currentPeriod,
+    maxPrice,
+    minPrice,
+    setMaxPrice,
+    setMinPrice,
+    setPeriod,
+  };
+}
