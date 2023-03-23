25d1331742049c100814e8cd5ba67bcaa9e12a27
Andrea <andreaacanfora95@gmail.com>
2023-03-23 15:14:02 +0100

    Revert "Adding loader"
    This reverts commit 29b1d9d81ec3f8339be571d48a49ff0616833704.

--- client/wagmilabsTradingTool/src/components/Modals/ProfitCalcModal/index.jsx
+++ client/wagmilabsTradingTool/src/components/Modals/ProfitCalcModal/index.jsx
@@ -1,27 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { Modal, ModalOverlay, ModalContent, ModalBody, ModalCloseButton } from "@chakra-ui/react";
-import { LoadingSpinner } from "@Components";
 
 import "./style.scss";
 
 export const ProfitCalcModal = React.memo(({ isOpen, setIsOpen }) => {
-  const [isIframeLoading, setIsIframeLoading] = useState(true);
-
   return (
     <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} size="full">
       <ModalOverlay />
       <ModalContent className="profit-calc-modal">
         <ModalCloseButton />
         <ModalBody>
-          {isIframeLoading && <LoadingSpinner width="100px" height="100px" margin="5rem 0" />}
-          <iframe
-            src="https://profitcalc.wagmilabs.tools?modalLayout=1"
-            title="Profit Calc"
-            onLoad={() => {
-              setIsIframeLoading(false);
-            }}
-            style={{ display: isIframeLoading ? "none" : "block" }}
-          />
+          <iframe src="https://profitcalc.wagmilabs.tools?modalLayout=1" title="Profit Calc" />
         </ModalBody>
       </ModalContent>
     </Modal>
