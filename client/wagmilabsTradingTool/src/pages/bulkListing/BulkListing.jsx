4ae1312f133652a5d842a5e5b6a3c70efc8b9e66
Federico <federicocavallini100@gmail.com>
2023-03-24 15:46:17 +0100

    add page title

--- client/wagmilabsTradingTool/src/pages/bulkListing/BulkListing.jsx
+++ client/wagmilabsTradingTool/src/pages/bulkListing/BulkListing.jsx
@@ -1,6 +1,7 @@
 import { PageWrapper } from "@Components";
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useSetPageTitle } from "../../custom-hooks";
 import { Header, ListCheckout, Table } from "./components";
 import { useItemData } from "./hooks/useItemData";
 import { useSelectAll } from "./hooks/useSelectAll";
@@ -8,6 +9,7 @@ import { useSelectAll } from "./hooks/useSelectAll";
 import "./style.scss";
 
 const BulkListing = () => {
+  useSetPageTitle("Bulk Listing || Wagmi labs");
   const { state } = useLocation();
   const { items, setMarketplace, setItems, changeListPrice, changeDuration } = useItemData(state);
   const {
