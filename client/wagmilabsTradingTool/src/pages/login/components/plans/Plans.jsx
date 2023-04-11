db959c4ecf327c438c56588483e888fb0fa7a4ea
Federico <federicocavallini100@gmail.com>
2023-04-11 17:16:01 +0200

    change plans and login video

--- client/wagmilabsTradingTool/src/pages/login/components/plans/Plans.jsx
+++ client/wagmilabsTradingTool/src/pages/login/components/plans/Plans.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import { Select } from "../../../../components/Form/Select";
-import { durationSelectOptions, planOptions, getDurationSelectors } from "./options";
+// import { Select } from "../../../../components/Form/Select";
+import {
+  durationSelectOptions,
+  planOptions,
+  // getDurationSelectors
+} from "./options";
 import Plan from "./Plan";
 import "./style.scss";
 
 const Plans = ({ plans }) => {
-  const [durationSelector, setDurationSelector] = React.useState(durationSelectOptions[0]);
-  const durationSelectors = getDurationSelectors();
+  const [durationSelector] = React.useState(durationSelectOptions[0]);
+  // const durationSelectors = getDurationSelectors();
 
   return (
     <div className={`plans-section ${plans && "plans-route"}`}>
       <div className="header">
-        <h2 className="title">Plans</h2>
+        <h2 className="title">Pricing</h2>
 
-        <Select
+        {/* <Select
           className="plan-selector"
           onChange={value => setDurationSelector(value)}
           label="Set duration"
           isSearchable={false}
           value={durationSelector}
           options={durationSelectors}
-        />
+        /> */}
       </div>
       <div className="plans">
         {planOptions.map(planOption => {
--- client/wagmilabsTradingTool/src/pages/login/components/plans/options.js
+++ client/wagmilabsTradingTool/src/pages/login/components/plans/options.js
@@ -7,30 +7,31 @@ export const planOptions = [
       3: 0,
       6: 0,
     },
-    description: `Try the starter plan subscription for FREE for 7 days. No hassle, no commitments. Includes starter plan features.`,
+    description: `Try the starter plan subscription for FREE for 7 days. No hassle, no commitments. Includes all features apart from bots.`,
     buttonName: "Start Trial",
   },
-  {
-    planId: 1,
-    name: "Starter Plan 💼",
-    prices: {
-      1: 0.06,
-      3: 0.05,
-      6: 0.045,
-    },
-    description:
-      "Starter plan includes all features apart from bots and wallet generator & manager. Get started today!",
-    buttonName: "Get Starter",
-  },
+  // {
+  //   planId: 1,
+  //   name: "Starter Plan 💼",
+  //   prices: {
+  //     1: 0.06,
+  //     3: 0.05,
+  //     6: 0.045,
+  //   },
+  //   description:
+  //     "Starter plan includes all features apart from bots and wallet generator & manager. Get started today!",
+  //   buttonName: "Get Starter",
+  // },
   {
     planId: 2,
     name: "Pro Plan 👑",
     prices: {
-      1: 0.09,
-      3: 0.075,
-      6: 0.06,
+      1: 0.03,
+      // 3: 0.075,
+      // 6: 0.06,
     },
-    description: "Pro Plan gives FULL ACCESS. Includes all starter plan features plus ALL Bots and Wallet Management.",
+    description:
+      "Pro Plan gives FULL ACCESS. Includes all our charts, bots, live views, tax tools, wallet manager and much more...",
     buttonName: "Get Pro",
   },
 ];
