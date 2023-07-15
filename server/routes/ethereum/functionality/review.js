5af152bf7834b7730b1b30724c4381244496e6af
Federico <federicocavallini100@gmail.com>
2023-07-15 14:56:49 +0200

    fix review

--- server/routes/ethereum/functionality/review.js
+++ server/routes/ethereum/functionality/review.js
@@ -8,18 +8,18 @@ reviewRoute.post("/review", async (req, res) => {
 
   if (!review) return res.status(400).json({ message: "No review found" });
   await insertStats({
-    address: null,
+    address: review,
     extra_data: null,
     ip_address: req.clientIp,
-    pass_type: review?.toString(),
+    pass_type: null,
     source: "website",
     timestamp: Date.now(),
     type: "review",
   });
   try {
-    res.status(200);
+    res.status(200).send({ message: "Review submitted" });
   } catch (err) {
-    res.status(400);
+    res.status(400).send({ message: "Error submitting review" });
   }
 });
 
